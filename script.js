16555a93e35bf9d1855dd4bde9f1e2ed23352cfc
yayascheer <yasmin.scheer@escola.pr.gov.br>
2025-07-01 18:36:19 +0000

    aula 03-04 e 05- alura

--- script.js
+++ script.js
@@ -3,5 +3,27 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    alert('fui clicado!');
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
+
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+    
+    return palavras;
 }
